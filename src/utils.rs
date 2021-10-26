211e6c6b217d5b9073fd83a72e110bfe6497e6c9
zkonge <zkonge@outlook.com>
2021-10-27 07:19:50 +0800

    Fix ownership

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ use interfaces::{Address, Interface};
 pub fn get_all_addresses() -> io::Result<Vec<Address>> {
     Ok(Interface::get_all()
         .expect("Unable to get local IP addresses")
-        .iter()
-        .flat_map(|f| f.addresses.iter())
+        .into_iter()
+        .flat_map(|f| f.addresses.clone().into_iter())
         .collect())
 }
